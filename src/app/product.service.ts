7689d47b6f3a37fbb78cb090618554d0a9c506da
outsource.phanupong <outsurce.phanupong@csc.co.th>
2017-10-10 01:32:04 +0700

    Add product list

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse} from  '@angular/common/http';
+import { HttpClient } from  '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class ProductService {
@@ -14,8 +12,7 @@ export class ProductService {
 
   getProducts(): Observable<Object[]>{
     return this._http.get<Object[]>(this._apiURL)
-              .do((data) => console.log(data))
-              .catch((err: HttpErrorResponse) => {console.log(err); return Observable.throw(err.error.message)});
+              .do((data) => console.log(data));
   }
 
 }
